aaa75e7318aed23eaeb3b3b3f95e88d10befc711
billionaredonation <5fermatop@gmail.com>
2026-05-12 00:55:31 +0200

    Update home.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -277,6 +277,7 @@ register('home', (root) => {
     <main class="home-gameplay">
       <section class="gta-map-stage">
         <div class="gta-map-bg"></div>
+        <div class="gta-stars"></div>
         <div class="gta-sky-light"></div>
 
         <div class="gta-water">
