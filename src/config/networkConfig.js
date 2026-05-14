07a3c0db062d170587a45279d862da24f59a96bd
billionaredonation <5fermatop@gmail.com>
2026-05-14 04:31:31 +0200

    Update networkConfig.js

--- src/config/networkConfig.js
+++ src/config/networkConfig.js
@@ -1,13 +1,14 @@
 export const NETWORK_CONFIG = {
   movement: {
     broadcastInterval: 25,
-    dbSaveInterval: 1200,
+    dbSaveInterval: 1500,
     heartbeatDelay: 1000,
+
     staleAfter: 5000,
     staleCheckInterval: 1000,
 
-    remoteSmoothing: 0.24,
-    remoteSnapDistance: 18,
+    remoteSmoothing: 0.28,
+    remoteSnapDistance: 35,
   },
 
   limits: {
