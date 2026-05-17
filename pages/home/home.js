c5f8940e1a3e6e993f16ec68b49184bae627521e
billionaredonation <5fermatop@gmail.com>
2026-05-17 05:53:04 +0200

    Update home.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -238,7 +238,7 @@ const cleanupMobilePrompt = setupMobileControlPrompt({
 const cleanupMapControls = enableMapControls(stage, viewport, {
   focusX: playerPosition.x,
   focusY: playerPosition.y,
-  startScale: isLowPowerDevice() ? 2.25 : 2.75,
+ startScale: isLowPowerDevice() ? 2.55 : 3.05,
 });
 
 const cleanupFogOfWar = enableFogOfWar({
