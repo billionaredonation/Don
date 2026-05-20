a5581931b4f29dac15f01b1b200a8914e4f5a3e3
billionaredonation <5fermatop@gmail.com>
2026-05-20 06:22:21 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ import '../pages/home/home.js';
 import { verifyTelegramAccess } from './auth/telegramAuth.js';
 
 
-localStorage.removeItem('mn-game-state');
-localStorage.removeItem('mn-mobile-controls-enabled');
 
 function renderTelegramOnlyScreen() {
   const root = document.getElementById('app');
