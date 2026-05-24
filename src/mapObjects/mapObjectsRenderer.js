e7edbc53f6bd7d0439d44ccb05ef079633c57f01
billionaredonation <5fermatop@gmail.com>
2026-05-24 10:04:28 +0200

    Update mapObjectsRenderer.js

--- src/mapObjects/mapObjectsRenderer.js
+++ src/mapObjects/mapObjectsRenderer.js
@@ -24,21 +24,22 @@ export function renderMapObjects(layer, objects = []) {
       const rotation = Number(object.rotation || 0);
       const selectedClass = object.selected ? 'map-object-selected' : '';
 
-      return `
-        <button
-          class="map-object map-object-${escapeHtml(object.category)} map-object-type-${escapeHtml(object.type)} ${selectedClass}"
-          data-map-object-id="${escapeHtml(object.id)}"
-          data-map-object-type="${escapeHtml(object.type)}"
-          data-map-object-category="${escapeHtml(object.category)}"
-          type="button"
-          title="${escapeHtml(object.name)}"
-          style="
-            left: ${x}%;
-            top: ${y}%;
-            --map-object-scale: ${scale};
-            --map-object-rotation: ${rotation}deg;
-          "
-        >
+          return `
+            <button
+              class="map-object map-object-${escapeHtml(object.category)} map-object-type-${escapeHtml(object.type)} ${selectedClass}"
+              data-map-object-id="${escapeHtml(object.id)}"
+              data-map-object-type="${escapeHtml(object.type)}"
+              data-map-object-category="${escapeHtml(object.category)}"
+              type="button"
+              tabindex="-1"
+              title="${escapeHtml(object.name)}"
+              style="
+              left: ${x}%;
+              top: ${y}%;
+              --map-object-scale: ${scale};
+              --map-object-rotation: ${rotation}deg;
+                "
+              >
           <span class="map-object-icon">${escapeHtml(object.icon || '◆')}</span>
         </button>
       `;
