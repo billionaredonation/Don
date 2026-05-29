6a7281d4d73efbc771339a3aa72c20a341934a94
billionaredonation <5fermatop@gmail.com>
2026-05-29 05:55:52 +0200

    Create houseActions.js

---
+++ src/entities/houseActions.js
@@ -0,0 +1,16 @@
+export function handleHouseAction(object) {
+  if (!object) return;
+
+  window.dispatchEvent(new CustomEvent('mn:house-selected', {
+    detail: {
+      house: object,
+      action: getHouseActionType(object),
+    },
+  }));
+}
+
+export function getHouseActionType(object) {
+  if (object?.payload?.ownerId) return 'info';
+  if (object?.payload?.locked) return 'locked';
+  return 'buy';
+}
