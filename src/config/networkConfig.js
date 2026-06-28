cc01ebb2e0afeddd90cb30ce622dc186b49a040f
billionaredonation <5fermatop@gmail.com>
2026-06-28 04:33:43 +0200

    Update networkConfig.js

--- src/config/networkConfig.js
+++ src/config/networkConfig.js
@@ -5,15 +5,15 @@ export const NETWORK_CONFIG = {
 
     // Presence / online
     heartbeatDelay: 1500,
-    presenceHeartbeatInterval: 4000,
-    onlineTtlMs: 18000,
+    presenceHeartbeatInterval: 3000,
+    onlineTtlMs: 16000,
 
     staleAfter: 18000,
-    staleCheckInterval: 2000,
+    staleCheckInterval: 1500,
 
     // Для снимка из БД даём запас, потому что updated_at сейчас
     // пишет клиент. Жёсткие 3-18 секунд ломали видимость при рассинхроне часов.
-    snapshotPlayerMaxAgeMs: 120000,
+    snapshotPlayerMaxAgeMs: 24000,
 
     remoteSmoothing: 0.16,
     remoteSnapDistance: 18,
