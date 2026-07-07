0074dff0d600436af722b9d8a69f4daab85bfce3
billionaredonation <5fermatop@gmail.com>
2026-07-07 07:16:28 +0200

    Update housesFeature.js

--- src/houses/housesFeature.js
+++ src/houses/housesFeature.js
@@ -140,6 +140,7 @@ export function enableHousesFeature(root, { cityId, city } = {}) {
 
     const result = await buyHouseFromState({
       houseId: getHouseId(house),
+      house,
       playerId: tgId,
     });
 
