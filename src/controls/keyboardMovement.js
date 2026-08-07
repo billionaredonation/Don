ad009e516f5d2995d52882b36a6321bf3ed08fcd
billionaredonation <5fermatop@gmail.com>
2026-08-07 22:40:19 +0200

    Update keyboardMovement.js

--- src/controls/keyboardMovement.js
+++ src/controls/keyboardMovement.js
@@ -132,7 +132,6 @@ export function enableKeyboardPlayerMovement(
       if (stamina >= STAMINA.recoveredAt) {
         const wasLocked = sprintLocked;
         sprintLocked = false;
-        stamina = STAMINA.max;
         if (wasLocked) {
           window.dispatchEvent(new CustomEvent('mn:player-stamina-recovered', {
             detail: { source: 'keyboard' },
