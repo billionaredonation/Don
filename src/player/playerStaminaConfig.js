0d0efad3b806c72925dfbfc986cb5df26e558ff9
billionaredonation <5fermatop@gmail.com>
2026-08-08 07:30:04 +0200

    Update playerStaminaConfig.js

--- src/player/playerStaminaConfig.js
+++ src/player/playerStaminaConfig.js
@@ -54,3 +54,4 @@ export function getStaminaRecoverySeconds(waterValue = 100) {
 export function getStaminaRecoveryPerFrame(waterValue = 100) {
   return PLAYER_STAMINA_CONFIG.max / (getStaminaRecoverySeconds(waterValue) * 60);
 }
+
