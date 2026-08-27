8e867b3e69c0b7a34998e943948b98a2ca8e8170
billionaredonation <5fermatop@gmail.com>
2026-08-27 07:41:55 +0200

    Update constructionConfig.js

--- src/construction/constructionConfig.js
+++ src/construction/constructionConfig.js
@@ -6,7 +6,7 @@ export const CONSTRUCTION_FACTORY_CONFIG = Object.freeze({
 });
 
 export const CONSTRUCTION_RECIPES = Object.freeze({
-  construction_board: Object.freeze({ id: 'construction_board', label: 'Обрезная доска', icon: '🪚', input: 'lumber_log', inputLabel: 'Брёвна', inputIcon: '🪵', inputQty: 2, outputQty: 8, seconds: 75, wage: 70, suggestedPrice: 95 }),
+  construction_board: Object.freeze({ id: 'construction_board', label: 'Обрезная доска', icon: '🪚', input: 'lumber_beam', inputLabel: 'Брус лесоруба', inputIcon: '▰', inputQty: 1, outputQty: 4, seconds: 75, wage: 70, suggestedPrice: 95 }),
   construction_timber: Object.freeze({ id: 'construction_timber', label: 'Строительный брус', icon: '▰', input: 'lumber_beam', inputLabel: 'Брус лесоруба', inputIcon: '▰', inputQty: 4, outputQty: 3, seconds: 60, wage: 60, suggestedPrice: 145 }),
   construction_plywood: Object.freeze({ id: 'construction_plywood', label: 'Фанерный лист', icon: '🟫', input: 'lumber_log', inputLabel: 'Брёвна', inputIcon: '🪵', inputQty: 3, outputQty: 5, seconds: 90, wage: 85, suggestedPrice: 125 }),
 });
