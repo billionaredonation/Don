f9b4b6c1348403cb2036773dbb4060d97c5177fc
billionaredonation <5fermatop@gmail.com>
2026-09-14 00:33:34 +0200

    Update woodProcessingFeature.js

--- src/woodProcessing/woodProcessingFeature.js
+++ src/woodProcessing/woodProcessingFeature.js
@@ -128,3 +128,4 @@ export function enableWoodProcessingFeature({ root, cityId } = {}) {
   window.addEventListener('mn:wood-processing-object-action', onObjectAction);
   return () => { window.removeEventListener('mn:wood-processing-object-action', onObjectAction); modal.remove(); };
 }
+
