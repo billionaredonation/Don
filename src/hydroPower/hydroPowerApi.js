1f3f9195e8bdcdb3dee4fe1180a815da4ad6c1f7
billionaredonation <5fermatop@gmail.com>
2026-09-21 01:54:53 +0200

    Update hydroPowerApi.js

--- src/hydroPower/hydroPowerApi.js
+++ src/hydroPower/hydroPowerApi.js
@@ -21,6 +21,8 @@ export const purchaseHydroEquipment = (plantId, cityId, equipment) => invoke('pu
 export const startHydroPlant = (plantId, cityId) => invoke('start', { plantId, cityId });
 export const stopHydroPlant = (plantId, cityId) => invoke('stop', { plantId, cityId });
 export const repairHydroPlant = (plantId, cityId) => invoke('repair', { plantId, cityId });
+export const addHydroBudget = (plantId, cityId, amount) => invoke('add_budget', { plantId, cityId, amount });
+export const withdrawHydroMoney = (plantId, cityId, amount) => invoke('withdraw', { plantId, cityId, amount });
 export const createHydroContract = (plantId, cityId, payload) => invoke('contract_create', { plantId, cityId, ...payload });
 
 export function getHydroError(error) {
@@ -31,7 +33,8 @@ export function getHydroError(error) {
     HYDRO_NOT_FOUND: 'ГЭС не найдена.', HYDRO_ALREADY_OWNED: 'У ГЭС уже есть владелец.', HYDRO_OWNER_REQUIRED: 'Действие доступно только владельцу.',
     PLAYER_BALANCE_NOT_ENOUGH: 'Недостаточно денег.', HYDRO_EQUIPMENT_ALREADY_PURCHASED: 'Этот узел инфраструктуры уже куплен.',
     HYDRO_LAUNCH_REQUIREMENTS: 'Для запуска нужны накопитель, ЛЭП и предохранители.', HYDRO_ALREADY_RUNNING: 'ГЭС уже запущена.',
-    HYDRO_NOT_RUNNING: 'ГЭС ещё не запущена.', HYDRO_CONDITION_LOW: 'Станция остановлена: требуется ремонт.',
+    HYDRO_NOT_RUNNING: 'ГЭС ещё не запущена.', HYDRO_CONDITION_LOW: 'Станция остановлена: требуется ремонт.', HYDRO_REPAIR_NOT_REQUIRED: 'Ремонт пока не требуется.',
+    HYDRO_AMOUNT_INVALID: 'Укажите корректную сумму.', HYDRO_BALANCE_NOT_ENOUGH: 'На счёте ГЭС недостаточно денег.',
     HYDRO_CONTRACT_AMOUNT_INVALID: 'Сумма контракта должна быть от 20 000 до 2 000 000 грн.', HYDRO_SUBSTATION_FUTURE_ONLY: 'Подстанции ещё не добавлены: контракт пока можно только подготовить.',
     ENERGY_CONTRACT_END_DATE_INVALID: 'Выберите дату окончания не раньше завтрашнего дня.',
   };
